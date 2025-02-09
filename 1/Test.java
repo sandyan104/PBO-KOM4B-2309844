702813a764e3997d766dbaca89013306f37f5f33
Sandy Nugraha <sandyan790@gmail.com>
2025-02-10 00:14:06 +0700

    Pertemuan 1

---
+++ 1/Test.java
@@ -0,0 +1,12 @@
+/**
+ *
+ * @author sandy
+ */
+
+package test;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Sandy Achmad Nugraha 2309844");
+    }
+}
